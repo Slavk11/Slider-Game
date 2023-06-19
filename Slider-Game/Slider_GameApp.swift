eb798f41a5a2d98700739314270d324d438a270c
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-19 21:46:15 +0300

    Initial Commit

---
+++ Slider-Game/Slider_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_GameApp.swift
+//  Slider-Game
+//
+//  Created by Сазонов Станислав on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Slider_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
