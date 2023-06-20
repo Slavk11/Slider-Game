1a178571865e668f87e546b5d6f0f829e0c5b9ca
Stanislav Sazonov <slavk11@yandex.ru>
2023-06-20 20:08:39 +0300

    Added State opacity , also add .onChange for user settings of slider

--- Slider-Game/SliderView.swift
+++ Slider-Game/SliderView.swift
@@ -8,27 +8,28 @@
 import SwiftUI
 
 struct SliderView: View {
+    @State private var opacity = 1.0
     @Binding var value: Int
     @Binding var targetValue: Int
-  
+    
     var body: some View {
-           UISliderRepresentation(value: $value, minimumValue: 0, maximumValue: 100)
-               .frame(width: 300, height: 50)
-               //.opacity(computeScore())
-       }
-       
-       private func computeScore() -> Double {
-           let difference = abs(targetValue - value)
-           return Double(100 - difference) / 100.0
-       }
-   }
-
-
+        UISliderRepresentation(value: $value, minimumValue: 0, maximumValue: 100)
+            .frame(width: 300, height: 50)
+            .opacity(opacity)
+            .onChange(of: value) { _ in
+                opacity = computeScore()
+            }
+    }
+    
+    private func computeScore() -> Double {
+        let difference = abs(targetValue - value)
+        return Double(100 - difference) / 100.0
+    }
+}
 
 struct SliderView_Previews: PreviewProvider {
     static var previews: some View {
         SliderView(value: .constant(20), targetValue: .constant(10))
     }
 }
-
 
